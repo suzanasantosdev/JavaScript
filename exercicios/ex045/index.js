387759da47ffb7e3c64f6b427fef64c8d4178bf1
Suzana Santos <suzanajb1@gmail.com>
2024-10-30 17:13:26 -0300

    finalizei o exercicio de try, catch e finally

--- exercicios/ex045/index.js
+++ exercicios/ex045/index.js
@@ -1,7 +1,48 @@
+// try {
+//     // e executada quando nao ha erros
+// } catch (e) {
+//     // e executada quando ha erros
+// } finally {
+//     // sempre
+// }
+
+/*------------------------------------------------------------*/
+// try {
+//     console.log('Abri um arquivo');
+//     console.log('Manipulei o arquivo e gerou erro');
+//     console.log('Fechei o arquiavo');
+
+//     try {
+//         console.log(b);
+//     } catch(e) {
+//         console.log('Deu erro');
+//     }
+// } catch (e) {
+//     console.log('Tratando o erro');
+// } finally {
+//     console.log('FINALLY: Eu sempre sou executado');
+// }
+
+function retornaHora(data) {
+    if (data && !(data instanceof Date)) {
+        throw new TypeError('Esperando instancia de Date.'); 
+    }
+
+    if (!data) {
+        data = new Date();
+    }
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false
+    });
+}
+
 try {
-    // e executada quando nao ha erros
-} catch (e) {
-    // e executada quando ha erros
+const data = new Date('07-12-1999 23:55:15');    
+const hora = retornaHora();
+console.log(hora);
+} catch(e) {
+    // Tratar erro
 } finally {
-    // sempre
+    console.log('Tenha um bom dia.');
 }
