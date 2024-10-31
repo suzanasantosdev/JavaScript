6d857c11778d0c3e644bbae01d85eacc92236756
Suzana Santos <suzanajb1@gmail.com>
2024-10-31 18:00:05 -0300

    finalizei projeto timer com setlnterval

--- exercicios/ex047/modelo/assets/js/main.js
+++ exercicios/ex047/modelo/assets/js/main.js
@@ -1,23 +1,43 @@
-function criaHoraDosSegundos(segundos) {
-    const data = new Date(segundos);
-    return data.toLocaleTimeString(pt-BR, {
-        hour12: false
-    });
-}
-
-const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
-
-iniciar.addEventListener('click', function(event) {
-    alert('Cliquei no iniciar');
-});
-
-pausar.addEventListener('click', function(event) {
-    alert('Cliquei no pausar');
-});
-
-zerar.addEventListener('click', function(event) {
-    alert('Cliquei no zerar');
-});
+    function relogio() {
+        function criaHoraDosSegundos(segundos) {
+        const data = new Date(segundos * 1000);
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'UTC'
+        });
+        }
+    
+        const relogio = document.querySelector('.relogio');
+        let segundos = 0;
+        let timer;
+    
+        function iniciaRelogio() {
+        timer = setInterval(function() {
+            segundos++;
+            relogio.innerHTML = criaHoraDosSegundos(segundos);
+        }, 1000);
+        }
+    
+        document.addEventListener('click', function(e) {
+        const el = e.target;
+    
+        if (el.classList.contains('zerar')) {
+            clearInterval(timer);
+            relogio.innerHTML = '00:00:00';
+            relogio.classList.remove('pausado');
+            segundos = 0;
+        }
+    
+        if (el.classList.contains('iniciar')) {
+            relogio.classList.remove('pausado');
+            clearInterval(timer);
+            iniciaRelogio();
+        }
+    
+        if (el.classList.contains('pausar')) {
+            clearInterval(timer);
+            relogio.classList.add('pausado');
+        }
+        });
+    }
+    relogio();
