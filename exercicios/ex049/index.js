e23d2d4bf31eeb713881e46f1b9096de21ebd139
Suzana Santos <suzanajb1@gmail.com>
2024-11-02 18:59:03 -0300

    finalizei o execicio de declarar funcoes de várias maneiras

--- exercicios/ex049/index.js
+++ exercicios/ex049/index.js
@@ -1,1 +1,34 @@
-// Declacao de funcao
+// Declacao de funcao (function hoistring)
+
+falaOi();
+
+function falaOi() {
+    console.log('Oi');
+}
+
+// First-class objects (objetos de primeira classe)
+// function expression
+const souUmDado = function () {
+    console.log('Sou Um dado.');
+};
+souUmDado();
+
+// function executaFuncao(funcao) {
+//     console.log('Vou executar sua funcao abaixo:');
+//     funcao();
+// }
+// executaFuncao(souUmDado)
+
+// Arrow function
+const funcaoArrow = () => {
+    console.log('Sou uma arrow function');
+};
+funcaoArrow();
+
+// Dentro de um objeto
+const obj = {
+    falar: function () {
+        console.log("Estou falando...");
+    }
+};
+obj.falar();
