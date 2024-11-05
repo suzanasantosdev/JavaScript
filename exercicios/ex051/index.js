46ea3ad7635457614a931afbc28347d9d39541f5
Suzana Santos <suzanajb1@gmail.com>
2024-11-05 20:44:10 -0300

    exercicio finalizado

--- exercicios/ex051/index.js
+++ exercicios/ex051/index.js
@@ -24,3 +24,17 @@
 // console.log(p2);
 
 
+function criaMultiplicador(multiplicador) {
+    // multiplicador
+    return function(n) {
+      return n * multiplicador;
+    };
+}
+    
+    const duplica = criaMultiplicador(2);
+    const triplica = criaMultiplicador(3);
+    const quadriplica = criaMultiplicador(4);
+    
+    console.log(duplica(3));
+    console.log(triplica(2));
+    console.log(quadriplica(10));
