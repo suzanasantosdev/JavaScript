a853db4ecfc6df59ad17d07f9951197203347b21
Suzana Santos <suzanajb1@gmail.com>
2024-11-05 20:03:27 -0300

    criei exercicio 051

---
+++ exercicios/ex051/index.js
@@ -0,0 +1,26 @@
+// return
+// retorna um valor
+// termina a funcao
+/*------------------------------------------------------------------------------ */
+// function soma(a, b) {
+//     return a + b; 
+// }
+
+// console.log(soma(5, 2));
+
+/*------------------------------------------------------------------------------- */
+
+// function criaPessoa(nome, sobrenome) {
+//     return {nome, sobrenome};
+// }
+
+// const p1 = criaPessoa('Suzana', 'Santos');
+// const p2 = {
+//     nome: 'David',
+//     sobrenome: 'Monteiro'
+// };
+
+// console.log(p1);
+// console.log(p2);
+
+
