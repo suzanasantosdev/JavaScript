7ad6d57989e2c2d579a002ef05a8cd7546ce93d7
Suzana Santos <suzanajb1@gmail.com>
2024-11-09 18:07:58 -0300

    exercicio finalizado

--- exercicios/ex056/index.js
+++ exercicios/ex056/index.js
@@ -1,8 +1,23 @@
-function criaPessoa(nome, sobrenome) {
+// factory function (funcao fabrica)
+// constructor function (funcao construtora)
+function criaPessoa(nome, sobrenome, a, p) {
     return {
-        nome, sobrenome
+        nome, 
+        sobrenome,
+        fala(assunto) {
+            return `${this.nome} está ${assunto}.`;
+        },
+        altura: a,
+        peso: p,
+        imc() {
+            const indice = this.peso / (this.altura ** 2);
+            return indice.toFixed(2);
+        }
     };
 }
 
-const p1 = criaPessoa('Suzana', 'Santos');
-console.log(p1);
+const p1 = criaPessoa('Suzana', 'Santos', 1.7, 60);
+console.log(p1.imc());
+const p2 = criaPessoa('David', 'Monteiro', 1.77, 85);
+console.log(p1. fala('Falando sobre JS'));
+console.log(p2.imc());
