856fbc0beecdf75a73e519edc6b41372c9bcfde7
Suzana Santos <suzanajb1@gmail.com>
2024-11-13 20:20:35 -0300

    arrays basicas

---
+++ exercicios/ex062/index.js
@@ -0,0 +1,11 @@
+// Valor por referência
+//                 0         1        2        3          4
+// const nomes = ['David', 'Suzana', 'Thiago', 'Suely', 'Rosana'];
+// const novo = nomes.slice(1, -2);
+// console.log(novo);
+
+// const nome = 'Suzana Santos Sousa';
+// const nomes = nome.split(' ');
+const nomes = [ 'Suzana', 'Santos', 'Sousa' ];
+const nome = nomes.join(' ');
+console.log(nome);
