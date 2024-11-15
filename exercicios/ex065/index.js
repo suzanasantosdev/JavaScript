bee84236c1d9febd634ef6bdf43996026b736b93
Suzana Santos <suzanajb1@gmail.com>
2024-11-15 20:39:40 -0300

    up

--- exercicios/ex065/index.js
+++ exercicios/ex065/index.js
@@ -3,10 +3,17 @@
 // Retorne os numeros maiores que 10
 //               0  1  2  3 4.......
 const numeros = [5,50,80,1,2,3,5,8,7,11,15,22,27];
+const numerosFiltrados = numeros.filter(valor => valor > 50);
+// console.log(numerosFiltrados);
 
-function callbackFilter(valor) {
-    return valor > 10;
-}
-
-const numerosFiltrados = numeros.filter(callbackFilter);
-console.log(numerosFiltrados);
+// Retorne as pessoas que tem o nome com 5 letras ou mais
+// Retorne as pessoas com mais de 50 anos
+// Retorne as pessoas cujo nome termina com a
+const pessoas = [
+    { nome: 'Luiz', idade: 62 },
+    { nome: 'Maria', idade: 23 },
+    { nome: 'Eduardo', idade: 55 },
+    { nome: 'Letícia', idade: 19 },
+    { nome: 'Rosana', idade: 32 },
+    { nome: 'Wallace', idade: 47 },
+];
