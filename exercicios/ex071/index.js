d43371ba42c99a02af6609b215c60f583e76871d
Suzana Santos <suzanajb1@gmail.com>
2024-11-22 16:36:23 -0300

    object defineProperty e properties

--- exercicios/ex071/index.js
+++ exercicios/ex071/index.js
@@ -1,4 +1,32 @@
 // defineProperty - definePropeties
 function Produto(nome, preco, estoque) {
 
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true, // mostra a chave
+        value: estoque,   // valor
+        writable: true,  // pode alterar
+        configurable: true  // configuravel
+    });
+
+    Object.defineProperties(this, {
+        nome: {
+            enumerable: true, // mostra a chave
+            value: nome,   // valor
+            writable: true,  // pode alterar
+            configurable: true  // configuravel
+        },
+        preco: {
+            enumerable: true, // mostra a chave
+            value: preco,   // valor
+            writable: true,  // pode alterar
+            configurable: true  // configuravel
+        },
+    });
+}
+
+const p1 = new Produto('Camiseta', 20, 3);
+console.log(p1);
+
+for(let chave in p1) {
+    console.log(chave);
 }
